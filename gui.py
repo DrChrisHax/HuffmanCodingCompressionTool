340bc26f584bf7cac0d46c0426fa80afd1395fd3
Jaxon McConnell <143143718+mcconnell-jaxon@users.noreply.github.com>
2025-03-20 11:44:09 -0700

    compression details now properly update

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 from tkinter import scrolledtext
 
 from Huffman import Encode, Decode, GetFileSize
+import os
 
 #root setup
 root = Tk()
@@ -11,23 +12,31 @@ root.title("Huffman Coding Compression Tool")
 root.geometry('450x400')
 
 def compress_button():
-    #opens file, runs encoder, updates the "huffman codes" text box
-    #as well as the compression details label
+    #opens file, runs encoder, updates the compression details label
+    #NEEDS TO: get the huffman codes in a way that can be interacted with
+    #          update row 3 text box
     file = openFile()
     compressed_file = file.replace(".txt", ".bin")
     Encode(file, compressed_file)
 
+    
+    detail.grid_forget()
+    details(file, compressed_file)
+
 def decompress_button():
+    #NEEDS TO: run decoder, check to see if decoded properly.
     pass
 
 def openFile():
     filepath = filedialog.askopenfilename()
-    file = open(filepath, 'r')
-    return file
-    
+    return filepath
 
-def details():
-    pass
+def details(file, output_file):
+    input_size = GetFileSize(file)
+    output_size = GetFileSize(output_file)
+    ratio = round(((output_size/input_size) * 100), 2)
+    detail = Label(root, text = f"Original: {input_size} bytes | Compressed: {output_size} | Ratio: {ratio}%", font=("Consolas", 8))
+    detail.grid(row = 2)
 
 #row 0, title label
 title = Label(root, text="Huffman Coding Compression Tool\n", font=("Consolas", 12))
@@ -39,8 +48,8 @@ select = Button(text ="Select Text File", font=("Consolas", 8), command=compress
 select.grid(row=1)
 
 #row 2, compression details label (tied to "compress_button" function)
-details = Label(root, text="Compression details will appear here", font=("Consolas", 8))
-details. grid(row = 2)
+detail = Label(root, text="Compression details will appear here", font=("Consolas", 8))
+detail.grid(row = 2)
 
 #row 3, "huffman codes" label
 l2 = Label(root, text="Huffman Codes", font=("Consolas", 10))
@@ -69,3 +78,4 @@ exit_ = Button(root, text='Exit', width=10, command=root.destroy)
 exit_.grid(row=8)
 
 root.mainloop()
+
